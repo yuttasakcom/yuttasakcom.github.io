0c57cfd8587d085be421da0a069c519542992f12
YoProgrammer <yuttasakcom@gmail.com>
2018-02-09 11:10:19 +0700

    init project

---
+++ webpack.config.babel.js
@@ -0,0 +1,41 @@
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+
+export default {
+  context: path.resolve(__dirname),
+  entry: {
+    app: './src'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.vue']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, '..', './src')]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true
+    })
+  ],
+  devServer: {
+    inline: true
+  }
+}
