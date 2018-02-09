fd75d0a60438810893388a7bbf95db07535b9ba6
YoProgrammer <yuttasakcom@gmail.com>
2018-02-09 22:03:08 +0700

    vue.runtime.common.js

--- src/components/Todos/Todos.vue
+++ src/components/Todos/Todos.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
         <ul>
-            <li :class="{complete: todo.done}" v-for="todo in todos.filter(shouldShowTodo)">
+            <li 
+                :class="{complete: todo.done}"
+                v-for="todo in todos.filter(shouldShowTodo)"
+                :key="todo.id">
                 {{ todo.text }}
                 <input type="checkbox" v-model="todo.done">
             </li>
@@ -17,81 +20,79 @@
 </template>
 
 <script>
-    import { mapGetters, mapActions } from 'vuex'
-    import TodoInput from './TodoInput'
-    export default {
-        components: {
-            TodoInput
-        },
-        computed: {
-            ...mapGetters(['todos', 'showDone'])
-        },
-        methods: {
-            ...mapActions(['setShowDone']),
-            shouldShowTodo(todo) {
-                if (this.showDone) {
-                    return true
-                } else {
-                    return !todo.done
-                }
-            },
-            showAll() {
-                this.setShowDone(!this.showDone)
-            }
-        }
+import { mapGetters, mapActions } from "vuex";
+import TodoInput from "./TodoInput";
+export default {
+  components: {
+    TodoInput
+  },
+  computed: {
+    ...mapGetters(["todos", "showDone"])
+  },
+  methods: {
+    ...mapActions(["setShowDone"]),
+    shouldShowTodo(todo) {
+      if (this.showDone) {
+        return true;
+      } else {
+        return !todo.done;
+      }
+    },
+    showAll() {
+      this.setShowDone(!this.showDone);
     }
+  }
+};
 </script>
 
 <style>
-    body {
-        font-family: Helvetica, sans-serif;
-        color: darkslategray;
-        font-size: 1.4em;
-        margin: 1.4em;
-        width: 22em;
-        position: relative;
-    }
+body {
+  font-family: Helvetica, sans-serif;
+  color: darkslategray;
+  font-size: 1.4em;
+  margin: 1.4em;
+  width: 22em;
+  position: relative;
+}
 
+input[type="checkbox"] {
+  position: absolute;
+  right: 0;
+  padding: 1em;
+  cursor: pointer;
+}
 
+li {
+  list-style-type: none;
+  padding: 1em 0 1em 0;
+  border-bottom: 1px solid lightblue;
+  position: relative;
+}
 
-    input[type=checkbox] {
-        position: absolute;
-        right: 0;
-        padding: 1em;
-        cursor: pointer;
-    }
+li input {
+  transform: scale(1.8);
+}
 
-    li {
-        list-style-type: none;
-        padding: 1em 0 1em 0;
-        border-bottom: 1px solid lightblue;
-        position: relative;
-    }
+ul {
+  padding: 0;
+}
 
-    li input {
-        transform: scale(1.8);
-    }
+label {
+  width: 80%;
+}
 
-    ul {
-        padding: 0;
-    }
+.complete {
+  color: gainsboro;
+  text-decoration: line-through;
+  font-style: italic;
+}
 
-    label {
-        width: 80%;
-    }
+.show-completed {
+  font-size: 1rem;
+  color: slategray;
+}
 
-    .complete {
-        color: gainsboro;
-        text-decoration: line-through;
-        font-style: italic;
-    }
-
-    .show-completed {
-        font-size: 1rem;
-        color: slategray;
-    }
-
-    .show-completed input {
-        transform: scale(1.4);
-    }
+.show-completed input {
+  transform: scale(1.4);
+}
 </style>
